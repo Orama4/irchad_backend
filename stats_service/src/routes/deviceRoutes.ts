2f91ba7434d15af020b0f3e25082dcfe89bac35c
lahcenbcf <h.bencharef@esi-sba.dz>
2025-03-15 10:04:25 +0100

    save

--- stats_service/src/routes/deviceRoutes.ts
+++ stats_service/src/routes/deviceRoutes.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import { getAllDevices,getDeviceStatistics,getDevicesMonth,getDevicesByFilters} from "../controllers/deviceController";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 
 const router = express.Router();
 
-router.get("/", getAllDevices);
-router.get("/stats", getDeviceStatistics);
-router.get("/monthly-stats", getDevicesMonth);
-router.get("/search", getDevicesByFilters); 
+router.get("/",authMiddleware,  getAllDevices);
+router.get("/stats", authMiddleware, getDeviceStatistics);
+router.get("/monthly-stats", authMiddleware, getDevicesMonth);
+router.get("/search", authMiddleware, getDevicesByFilters); 
 //http://localhost:5001/api/devices/search?status=connected for status or 
 //http://localhost:5001/api/devices/search?type=ceinture for type   
 //This will be used both in recherche (search by advice type) and filtering (filter by status)            

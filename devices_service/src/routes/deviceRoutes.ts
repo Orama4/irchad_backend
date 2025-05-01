880aa025ffc00e0cb3bd5eef40dec8fcd53361f5
Bouhadi haifaa <lh_bouhadi@esi.dz>
2025-05-01 15:07:05 +0100

    removed an unecessary route

--- devices_service/src/routes/deviceRoutes.ts
+++ devices_service/src/routes/deviceRoutes.ts
@@ -14,13 +14,12 @@ const router = express.Router();
 router.post('/control', controlDevice);
 router.get("/",/*authMiddleware,*/);
 // Device control routes
-router.get('/status/:deviceId', requestDeviceStatus);  // Get device status
+router.get('/status/:deviceId', requestDeviceStatus);  //sends a new request to the device for its status
 router.post('/subscribe/:deviceId', subscribeDeviceUpdates); // Subscribe to device updates
 
 // Heartbeat data routes
-router.get('/heartbeat/:deviceId', getDeviceHeartbeat);     // Get last known heartbeat for a device
+router.get('/heartbeat/:deviceId', getDeviceHeartbeat);  //first checks cached data, then requests new data if needed
 router.get('/risky-devices', getRiskyDevices);      // Get all risky devices
-router.post('/refresh-heartbeat/:deviceId', refreshDeviceHeartbeat);  // Force refresh of device heartbeat
 
 
 router.post("/");
